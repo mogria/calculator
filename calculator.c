3c188ef33f6db088636100728a8cc174c07bb679
Mogria <m0gr14@gmail.com>
2011-12-28 13:40:39 +0100

    added struct for an simple expression

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Expression {
+  int zahl1;
+  Expression *expression1;
+  char operator;
+  int zahl2;
+  Expression *expression2;
+} Expression;
+
+int main() {
+  
+  return 0;
+}
