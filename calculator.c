f4513dff209f8a520b75a9a24ac3f7363c25f0b2
Mogria <m0gr14@gmail.com>
2011-12-28 14:10:28 +0100

    commented a bit, added process funciton

--- calculator.c
+++ calculator.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> // input and output functions
+#include <stdlib.h> // atoi, etc ...
+#include <string.h> // string functions
 
 #define MAX_INPUT 80
 
@@ -12,13 +12,41 @@ typedef struct Expression {
   struct Expression *expression2;
 } Expression;
 
+float process ( char *Input );
+
 int main() {
-  char Input[MAX_INPUT];
-  char Format[20];
+  char Input[MAX_INPUT]; // input of the user
+  char Format[20]; // format of the input
+  short More;
+
+  // Intro
+  printf ( "Initializing Calculator 15 ...\n" );
+
+  // Generating the format of the input string according to MAX_INPUT
+  sprintf ( Format, "%%%is", MAX_INPUT );
+
   do {
-    sprintf ( Format, "%%%is", MAX_INPUT );
+    // printing the shell prompt
+    printf ( "c15> " );
+
+    // reading the input
     scanf ( Format, Input );
-  } while ( strlen(Input) != 0 );
+
+    // only process input if Input is not empty
+    if ( More = ( strcmp ( Input, "exit" ) != 0 ) ) {
+      printf ( "=> %f\n", process ( Input ) );
+    }
+    printf ( "more: %i \n", More );
+
+    // end programm if input is empty
+  } while ( More );
+
+  // bye :D
   puts ( "bye!" );
   return 0;
+}
+
+// function which processes the input and returns the result das double
+float process ( char *Input ) {
+  return atof ( Input );
 }
