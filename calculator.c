e4a1940a747fabfc8341d48df5e825cf531b7d0e
Mogria <m0gr14@gmail.com>
2011-12-28 13:58:14 +0100

    Input system completed

--- calculator.c
+++ calculator.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT 80
 
 typedef struct Expression {
   int zahl1;
-  Expression *expression1;
+  struct Expression *expression1;
   char operator;
   int zahl2;
-  Expression *expression2;
+  struct Expression *expression2;
 } Expression;
 
 int main() {
-  
+  char Input[MAX_INPUT];
+  char Format[20];
+  do {
+    sprintf ( Format, "%%%is", MAX_INPUT );
+    scanf ( Format, Input );
+    printf("%s:%s", Format, Input );
+  } while ( strlen(Input) != 0 );
+  puts ( "bye!" );
   return 0;
 }
